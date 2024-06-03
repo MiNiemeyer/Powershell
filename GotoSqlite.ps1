30d6abae1847f16b766f973ff52eeff44552e5d8
MiNiemeyer <mni_col@hotmail.com>
2024-06-03 16:15:28 +0200

    2024_06_03

---
+++ GotoSqlite.ps1
@@ -0,0 +1,15 @@
+﻿[Reflection.Assembly]::LoadFile("C:\Temp\System.Data.SQLite.dll")
+
+
+$sDatabasePath = "G:\Daten\EDV\Test_OH\pdfmailer6\license.dat"
+
+$sConnectionString = "Data Source=$sDatabasePath"
+
+$SQLiteConnection = New-Object System.Data.SQLite.SQLiteConnection 
+
+<#$SQLiteConnection.ConnectionString = $sConnectionString
+
+$SQLiteConnection.Open()
+
+$SQLiteConnection.Close()*/
+#>
